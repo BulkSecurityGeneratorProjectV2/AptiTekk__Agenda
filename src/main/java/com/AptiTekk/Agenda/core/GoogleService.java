5e9b8abadbe92b04742247e7740ecc071602962b
Kevin Thorne <kevint@thedigitalnative.space>
2016-05-16 09:44:16 -0600

    Google Calendar Service implmented

--- src/main/java/com/AptiTekk/Agenda/core/GoogleService.java
+++ src/main/java/com/AptiTekk/Agenda/core/GoogleService.java
@@ -3,45 +3,34 @@ package com.AptiTekk.Agenda.core;
 import com.AptiTekk.Agenda.core.entity.AppProperty;
 import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.util.store.DataStoreFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.calendar.Calendar;
-import java.io.File;
-import java.io.IOException;
+
 import javax.ejb.Local;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Local
 public interface GoogleService {
-    
-    public static final String APP_NAME = "Agenda";
-    
-    public static final String CALENDAR_USER_ID = "CALENDAR";
-    
-    public static final String INITAL_PATH = "/CalendarServlet";
-    public static final String CALLBACK_PATH = "/CalendarCallbackServlet";
-    
-    public static final AppProperty ACCESS_TOKEN_PROPERTY = new AppProperty("agenda.google.calendar.accessToken","");
-    public static final AppProperty CALENDAR_ID_PROPERTY = new AppProperty("agenda.google.calendar.calendarID", "primary");
-    
-    public static final AppProperty CLIENT_ID_PROPERTY = new AppProperty("agenda.google.clientID", "457451349060-uo39jdu88fbm4ib0r7l6jrba3k3uflem.apps.googleusercontent.com");
-    public static final AppProperty CLIENT_SECRET_PROPERTY = new AppProperty("agenda.google.clientSecret", "YNNu0ompxxjoZRZVRvK3GPhY");
 
-    public Credential authorize(String userId) throws IOException;
+    String APP_NAME = "Agenda";
 
-    public Calendar getCalendarService() throws IOException;
+    String INITAL_PATH = "/CalendarServlet";
+    String CALLBACK_PATH = "/CalendarCallbackServlet";
+
+    AppProperty ACCESS_TOKEN_PROPERTY = new AppProperty("agenda.google.calendar.accessToken", "");
+
+    AppProperty CLIENT_ID_PROPERTY = new AppProperty("agenda.google.clientID", "457451349060-uo39jdu88fbm4ib0r7l6jrba3k3uflem.apps.googleusercontent.com");
+    AppProperty CLIENT_SECRET_PROPERTY = new AppProperty("agenda.google.clientSecret", "YNNu0ompxxjoZRZVRvK3GPhY");
+
+    Credential authorize(String userId) throws IOException;
     
     AuthorizationCodeFlow initializeCalendarFlow() throws IOException;
     
     String getCalendarRedirectUri(HttpServletRequest req) throws ServletException, IOException;
     
     String getCalendarUserId(HttpServletRequest req) throws ServletException, IOException;
-    
-    public DataStoreFactory getDataStoreFactory() throws IOException;
-    
-//    return new FileDataStoreFactory(new File(System.getProperty("jboss.server.data.dir"), "Agenda_GoogleCredentials"));
+
+    DataStoreFactory getDataStoreFactory() throws IOException;
 
 }
--- src/main/java/com/AptiTekk/Agenda/core/impl/GoogleServiceImpl.java
+++ src/main/java/com/AptiTekk/Agenda/core/impl/GoogleServiceImpl.java
@@ -5,30 +5,27 @@
  */
 package com.AptiTekk.Agenda.core.impl;
 
-import static com.AptiTekk.Agenda.core.GoogleService.CALLBACK_PATH;
+import com.AptiTekk.Agenda.core.GoogleCalendarService;
+import com.AptiTekk.Agenda.core.GoogleService;
 import com.AptiTekk.Agenda.core.Properties;
 import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.calendar.Calendar;
+import com.google.api.client.util.store.DataStoreFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.CalendarScopes;
-import java.io.IOException;
-import java.util.Collections;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import com.AptiTekk.Agenda.core.GoogleService;
-import com.AptiTekk.Agenda.core.utilities.AgendaLogger;
-import com.google.api.client.util.store.DataStoreFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
 
 /**
  *
@@ -40,17 +37,11 @@ public class GoogleServiceImpl implements GoogleService {
     @Inject
     Properties properties;
 
-    HttpTransport httpTransport;
-    JsonFactory jsonFactory;
-
     String clientID;
     String clientSecret;
 
     @PostConstruct
     public void init() {
-        httpTransport = new NetHttpTransport();
-        jsonFactory = new JacksonFactory();
-
         clientID = properties.get(CLIENT_ID_PROPERTY.getKey());
         clientSecret = properties.get(CLIENT_SECRET_PROPERTY.getKey());
     }
@@ -58,20 +49,6 @@ public class GoogleServiceImpl implements GoogleService {
     @Override
     public Credential authorize(String userId) throws IOException {
         return initializeCalendarFlow().loadCredential(userId);
-    }
-
-    @Override
-    public Calendar getCalendarService() throws IOException {
-        Credential cred = authorize(CALENDAR_USER_ID);
-        if (cred == null) { //reauthorize
-            AgendaLogger.logMessage("Google Calendar Credential was null");
-            return null;
-        } else {
-            return new com.google.api.services.calendar.Calendar.Builder(
-                    httpTransport, jsonFactory, cred)
-                    .setApplicationName(APP_NAME)
-                    .build();
-        }
     }
 
     @Override
@@ -94,12 +71,11 @@ public class GoogleServiceImpl implements GoogleService {
 
     @Override
     public String getCalendarUserId(HttpServletRequest req) throws ServletException, IOException {
-        return CALENDAR_USER_ID;
+        return GoogleCalendarService.CALENDAR_USER_ID;
     }
 
     @Override
     public DataStoreFactory getDataStoreFactory() throws IOException {
-//        return new JdoDataStoreFactory(persistenceManagerFactory);
         return new FileDataStoreFactory(new File(System.getProperty("jboss.server.data.dir"), "Agenda_GoogleCredentials"));
     }
 }
