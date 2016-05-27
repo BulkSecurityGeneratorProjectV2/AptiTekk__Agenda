e960b3f77fb689277fb053e715e5aa5c5b8416a3
Mitch Talmadge <mitch@mitchtalmadge.com>
2016-05-27 13:22:46 -0600

    Fixed logic errors preventing user credentials test from passing.

--- src/main/java/com/AptiTekk/Agenda/core/utilities/EqualsHelper.java
+++ src/main/java/com/AptiTekk/Agenda/core/utilities/EqualsHelper.java
@@ -1,17 +1,33 @@
 package com.AptiTekk.Agenda.core.utilities;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class EqualsHelper {
 
-    public static boolean areEquals(Object var1, Object var2)
-    {
-        return (var1 == var2) || (var1 != null && var2 != null && var1.equals(var2));
+    public static boolean areEquals(Object var1, Object var2) {
+        if (var1 == var2)
+            return true;
+
+        if (var1 != null && var2 != null) {
+            if (var1.equals(var2)) {
+                return true;
+            }
+            else if (var1.getClass().isArray() && var2.getClass().isArray()) {
+                for (int i = 0; i < Array.getLength(var1); i++) {
+                    if (!(Array.get(var1, i).equals(Array.get(var2, i)))) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
     }
 
-    public static int calculateHashCode(Object... objects)
-    {
+    public static int calculateHashCode(Object... objects) {
         int hashCode = 0;
-        for(Object object : objects)
-        {
+        for (Object object : objects) {
             hashCode += (object == null ? 0 : object.hashCode());
         }
 
