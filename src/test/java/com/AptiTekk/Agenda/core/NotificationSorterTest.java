66f90451ded652b32d8d212423796145f491284e
Kevin Thorne <kevint@thedigitalnative.space>
2016-05-29 14:47:25 -0600

    NotificationSorterTest now comparing to correct assertion

--- src/test/java/com/AptiTekk/Agenda/core/NotificationSorterTest.java
+++ src/test/java/com/AptiTekk/Agenda/core/NotificationSorterTest.java
@@ -1,7 +1,6 @@
 package com.AptiTekk.Agenda.core;
 
 import com.AptiTekk.Agenda.core.entity.*;
-import com.AptiTekk.Agenda.core.impl.NotificationServiceImpl;
 import com.AptiTekk.Agenda.core.testingUtil.TestUtils;
 import com.AptiTekk.Agenda.core.utilities.notification.NotificationListener;
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -12,8 +11,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 @RunWith(Arquillian.class)
@@ -38,22 +37,10 @@ public class NotificationSorterTest {
 
         userService.insert(testUser);
 
-        Notification unreadNotification1 = new Notification();
-        unreadNotification1.setCreation(new Date());
-        unreadNotification1.setSubject("unreadNotification1");
-        unreadNotification1.setRead(false);
-        unreadNotification1.setUser(testUser);
-
-        Notification readNotification1 = new Notification();
-        readNotification1.setCreation(new Date());
-        readNotification1.setRead(true);
-        readNotification1.setSubject("readNotification1");
-        readNotification1.setUser(testUser);
-
         Notification unreadNotification2 = new Notification();
         unreadNotification2.setCreation(new Date());
-        unreadNotification2.setRead(false);
         unreadNotification2.setSubject("unreadNotification2");
+        unreadNotification2.setRead(false);
         unreadNotification2.setUser(testUser);
 
         Notification readNotification2 = new Notification();
@@ -61,6 +48,18 @@ public class NotificationSorterTest {
         readNotification2.setRead(true);
         readNotification2.setSubject("readNotification2");
         readNotification2.setUser(testUser);
+
+        Notification unreadNotification1 = new Notification();
+        unreadNotification1.setCreation(new Date());
+        unreadNotification1.setRead(false);
+        unreadNotification1.setSubject("unreadNotification1");
+        unreadNotification1.setUser(testUser);
+
+        Notification readNotification1 = new Notification();
+        readNotification1.setCreation(new Date());
+        readNotification1.setRead(true);
+        readNotification1.setSubject("readNotification1");
+        readNotification1.setUser(testUser);
 
         notificationService.insert(unreadNotification1);
         notificationService.insert(readNotification1);
@@ -71,7 +70,7 @@ public class NotificationSorterTest {
         List<Notification> notifs = notificationService.getAllByUser(testUser);
 
         //assert
-        List<Notification> expected = new ArrayList<Notification>() {{
+        List<Notification> expected = new LinkedList<Notification>() {{
             add(unreadNotification1);
             add(unreadNotification2);
             add(readNotification1);
