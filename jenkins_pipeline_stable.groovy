48d6b7cefdbd6b1e6228194ef35980b953c2269d
Mitch Talmadge <mitch@mitchtalmadge.com>
2016-06-29 05:52:20 -0600

    Added variable for agendaqa URL to pipeline

--- jenkins_pipeline_stable.groovy
+++ jenkins_pipeline_stable.groovy
@@ -1,4 +1,6 @@
 node {
+    def agendaqaUrl = "https://agendaqa-aptitekk.rhcloud.com"
+
     try {
         stage 'Checkout Stable'
         checkout([$class: 'GitSCM', branches: [[name: '*/stable']], browser: [$class: 'GogsGit'], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '542239bb-3d63-40bc-9cfa-e5ed56a1fc5b', url: 'ssh://git@core.aptitekk.com:28/AptiTekk/Agenda.git']]])
@@ -16,7 +18,7 @@ node {
         def i = 0
 
         while (i < 10) {
-            sh "curl -o /dev/null --silent --head --write-out '%{http_code}' https://agendaqa-aptitekk.rhcloud.com > .agendaqa-status"
+            sh "curl -o /dev/null --silent --head --write-out '%{http_code}' ${agendaqaUrl} > .agendaqa-status"
             def status = readFile '.agendaqa-status'
 
             if (status == "200") {
@@ -27,15 +29,15 @@ node {
             i++
         }
 
-        if(i == 10) {
+        if (i == 10) {
             error 'Could not connect to agenda qa deployment after 5 minutes. Did it deploy?'
         }
 
         stage 'Stable QA'
-        slackSend color: 'good', message: 'A new QA build is ready for testing! Access it here: https://agendaqa-aptitekk.rhcloud.com/'
+        slackSend color: 'good', message: 'A new QA build is ready for testing! Access it here: ' + agendaqaUrl
         input 'Please test https://agendaqa-aptitekk.rhcloud.com/ and proceed when ready.'
-    } catch(err) {
-        slackSend color: 'danger', message: 'An Error occurred during the Agenda Stable Pipeline. Error: '+err
+    } catch (err) {
+        slackSend color: 'danger', message: 'An Error occurred during the Agenda Stable Pipeline. Error: ' + err
         error err
     }
 }
